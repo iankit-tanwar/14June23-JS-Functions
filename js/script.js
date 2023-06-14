d30b6640755a0ed20ece58c7ac8e35dc10a42a5a
iankit-tanwar <tankitofficial01.gmail.com>
2023-06-14 10:15:02 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+
+//1 Functions definations is the one time process
+function greetMyStudents(name){ // I can receive some formal argument
+
+console.log("hello"+" "+name ); 
+
+}
+
+//2 function calling is a many time process 
+// invoke /call     invoking/calling
+
+
+greetMyStudents('Ankit');  // we can pass some actual arguments 
+greetMyStudents(`Praveen`);
+greetMyStudents("Sunil");
---
+++ js/script2.js
@@ -0,0 +1,18 @@
+// Add some Numbers with the help of functions
+
+
+//1 function defination is one time process 
+
+
+function addNumber(n1 , n2){  // formal arguments receive
+
+    console.log(n1 + n2);
+}
+
+
+//2 function calling is many time process 
+
+addNumber(10,20); // actual arguments passing 
+addNumber(10,50); // actual arguments passing
+addNumber(90,20); // actual arguments passing
+
